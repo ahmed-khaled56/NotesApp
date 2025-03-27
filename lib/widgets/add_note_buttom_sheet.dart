fcf57ed049a989f18d293b21829e31acc503527d
ahmed <mhmamdmf275@gmail.com>
2025-03-28 01:30:23 +0200

    manage state by add bloc consumer to bottom sheet

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_notes_cubit/add_notes_cubits.dart';
 import 'package:notes_app/widgets/Add_Note_form.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_textField.dart';
@@ -16,7 +19,24 @@ class _AddNoteButtomSheetState extends State<AddNoteButtomSheet> {
     return Container(
       child: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: SingleChildScrollView(child: AddNoteForm()),
+        child: SingleChildScrollView(
+          child: BlocConsumer<NotesAddCubits, AddNotesStates>(
+            listener: (context, state) {
+              if (state is SuccessfulAddNOtesState) {
+                Navigator.pop(context);
+              } else if (state is FailureAddNOtesState) {
+                print('failed ${state.errorMessage}');
+              }
+            },
+
+            builder: (context, state) {
+              return ModalProgressHUD(
+                inAsyncCall: state is LoadingAddNOtesState ? true : false,
+                child: AddNoteForm(),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
