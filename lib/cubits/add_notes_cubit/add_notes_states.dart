24ea7cd333cf1cbdb41fe6fa5169ec33d4163fa7
ahmed <mhmamdmf275@gmail.com>
2025-03-27 22:46:46 +0200

    define states of add note

--- lib/cubits/add_notes_cubit/add_notes_states.dart
+++ lib/cubits/add_notes_cubit/add_notes_states.dart
@@ -3,3 +3,13 @@ part of "add_notes_cubits.dart";
 abstract class AddNotesStates {}
 
 class initialAddNOtesState extends AddNotesStates {}
+
+class LoadingAddNOtesState extends AddNotesStates {}
+
+class SuccessfulAddNOtesState extends AddNotesStates {}
+
+class FailureAddNOtesState extends AddNotesStates {
+  final String errorMessage;
+
+  FailureAddNOtesState(this.errorMessage);
+}
